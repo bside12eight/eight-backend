1f57f7f8ea72e0abdbeb3cb6b1c524c6930a0bf2
kyumin <kyumin.choi@nhnfashiongo.com>
2022-09-25 10:42:14 +0900

    first commit

---
+++ src/main/java/com/example/bsidetest/BsideTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bsidetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsideTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsideTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bsidetest/BsideTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsidetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsideTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
