4b29aa576ad8d065065c0be2f6a008ec2d053326
haenyilee <haenyilee@kakao.com>
2022-09-27 22:24:36 +0900

    Etc: 프로젝트 생성

---
+++ DroptheClothes/src/main/java/com/droptheclothes/api/DroptheClothesApplication.java
@@ -0,0 +1,13 @@
+package com.droptheclothes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroptheClothesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DroptheClothesApplication.class, args);
+  }
+
+}
